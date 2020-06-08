aaa981b5ca538697fe66df8ed88b696916d2f76a
Beck L <11636145+blorsch@users.noreply.github.com>
2020-06-07 17:23:15 -0700

    Initial Commit

---
+++ MarinTrace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarinTrace
+//
+//  Created by Beck Lorsch on 6/7/20.
+//  Copyright © 2020 Couch Potato Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
