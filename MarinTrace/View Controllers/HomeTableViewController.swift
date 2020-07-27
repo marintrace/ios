17284ac8ba1019483ce91f801209b97865f76614
Beck L <11636145+blorsch@users.noreply.github.com>
2020-07-27 11:05:22 -0700

    minor visual fix when loading home as a new user

--- MarinTrace/View Controllers/HomeTableViewController.swift
+++ MarinTrace/View Controllers/HomeTableViewController.swift
@@ -17,6 +17,7 @@ class HomeTableViewController: UITableViewController {
         super.viewDidLoad()
         checkUser()
         setupTableView()
+        configViewsForUser()
     }
     
     func setupTableView() {
